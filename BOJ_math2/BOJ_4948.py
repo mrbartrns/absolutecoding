363e201418fa3a3ed45b29b12d4b8de24f6ec5ad
Junhyuck Ko <mrbartrns@gmail.com>
2019-12-04 22:57:40 +0900

    베르트랑 공준

---
+++ BOJ_math2/BOJ_4948.py
@@ -0,0 +1,22 @@
+#에네토스테리스의 채 응용, 베르트랑 공준
+import sys
+
+def prime_list(n):
+    flag = [True] * (2 * n + 1)
+    flag[1] = False
+    x = int((2 * n) ** (1 / 2)) + 1
+    for i in range(2, x):
+        if flag[i]:
+            for j in range(i + i, 2 * n + 1, i):
+                flag[j] = False
+
+    lst1 = [i for i in range(n + 1 , 2 * n + 1) if flag[i] == True]
+    return len(lst1)
+
+chk = True
+while chk:
+    a = int(sys.stdin.readline())
+    if a == 0:
+        chk = False
+    else:
+        print(prime_list(a))
