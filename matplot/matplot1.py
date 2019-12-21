ca6749f77a1c2fb9c6994563b5c54e1cb6547e8b
Junhyuck Ko <mrbartrns@gmail.com>
2019-12-21 09:22:45 +0900

    matplot 추가

--- matplot/matplot1.py
+++ matplot/matplot1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def solve(lst):
-    plt.plot(lst)
+    plt.plot(lst)  # (x축, y축, label = 순으로 생성됨. 하나의 변수만 존재할 경우 lst의 번수가 x축이 됨)
 
     plt.ylabel('some numbers')  # y축의 이름을 나타냄.
 
