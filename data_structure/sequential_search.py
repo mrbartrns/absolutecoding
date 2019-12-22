fee5516e126e04671947a18988091d662c906c47
Junhyuck Ko <mrbartrns@gmail.com>
2019-12-23 08:41:14 +0900

    순차탐색 1

--- data_structure/sequential_search.py
+++ data_structure/sequential_search.py
@@ -10,5 +10,17 @@ def sequential_search1(arr, val):
     else:
         print(val, arr[0] == val)
 
+def sequential_search2(arr, val):
+    num = 0
+    flag = True
+    if len(arr) > 1:
+        while flag:
+            if arr[num] == val:
+                print(val, 'True')
+                flag = False
+            else:
+                num += 1
+
+
 a = [2, 3, 6, 7, 10, 13]
 sequential_search1(a, 13)
