557aaf5019ba490f53e6c4ba03a7a63360466d10
Junhyuck Ko <mrbartrns@gmail.com>
2019-12-26 19:02:41 +0900

    하노이의 탑 풀이

--- recursive/BOJ_11729.py
+++ recursive/BOJ_11729.py
@@ -2,14 +2,24 @@
 import sys
 
 
-def func(n):
+def hanoi(n, from_pos=1, to_pos=3, aux_pos=2):
+    # n이 1일때, from과 to를 출력
     if n == 1:
-        print('*')
+        print(from_pos, to_pos)
     else:
-        func(n - 1)
-        print('*' * n)
-        func(n - 1)
+        # n - 1개의 원판을 from에서 aux로 이동 시킴
+        hanoi(n - 1, from_pos, aux_pos, to_pos)
+        # 맨 아래의 원판을 from에서 to로 이동시킴
+        print(from_pos, to_pos)
+        # aux에 있는 n - 1 개의 원판을 to로 이동시킴
+        hanoi(n - 1, aux_pos, to_pos, from_pos)
 
-#수정중
-func(5)
-#별찍기 연습중
+
+n = int(sys.stdin.readline())
+total = 0
+for i in range(n):
+    total *= 2
+    total += 1
+
+print(total)
+hanoi(n)
