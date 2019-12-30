9d6daed5eff81ce874a83af48bb48367176de858
Junhyuck Ko <mrbartrns@gmail.com>
2019-12-30 23:33:06 +0900

    selection sort

--- data_structure/selection_sort.py
+++ data_structure/selection_sort.py
@@ -5,17 +5,18 @@ import sys
 def swap(arr, first, second):
     arr[first], arr[second] = arr[second], arr[first]
 
-def selection_sort(arr, startIndex=0):
-    for i in range(startIndex, len(arr)):
-        minimumIndex = startIndex
-        minimumValue = arr[startIndex]
-        for j in range(startIndex, len(arr)):
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        minimumIndex = i
+        minimumValue = arr[i]
+        for j in range(i, len(arr)):
             if minimumValue >= arr[j]:
-                minimumIndex = i
-                minimumValue = arr[i]
-                swap(arr, startIndex, minimumIndex)
-        startIndex += 1
+                minimumIndex = j
+                minimumValue = arr[j]
+        swap(arr, i, minimumIndex)
     return arr
+
 
 arr = [3, 4, 5, 6, 1, 8, 9, 7, 2]
 print(selection_sort(arr))
